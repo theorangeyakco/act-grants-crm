018cca65f00338bc48a6a54044116d559bdabb1b
Param Kapur <paramkapur2002@gmail.com>
2021-04-30 22:08:44 -0700

    fix?

--- donations/urls.py
+++ donations/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.views.decorators.csrf import csrf_exempt
 from rest_framework.routers import DefaultRouter
 
 from donations.views import AcceptWebhook
@@ -7,5 +8,5 @@ from donations.views import AcceptWebhook
 app_name = 'donations'
 
 urlpatterns = [
-	path('payment/new', AcceptWebhook.as_view(), name='accept_webhook'),
+	path('payment/new', csrf_exempt(AcceptWebhook.as_view()), name='accept_webhook'),
 ]
