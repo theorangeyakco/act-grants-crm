f453a9fa8f6989a1f357a7c77bd9a9b83b287a5a
Param Kapur <paramkapur2002@gmail.com>
2021-04-30 21:27:19 -0700

    changes

--- donations/views.py
+++ donations/views.py
@@ -20,22 +20,23 @@ class AcceptWebhook(APIView):
 
 	@staticmethod
 	def post(request):
-		data = request.POST
+		client = razorpay.Client(auth=(os.getenv('RZP_KEY'), os.getenv("RZP_SECRET_KEY")))
+		try:
+			client.utility.verify_webhook_signature(request.data, request.headers['X-Razorpay-Signature'],
+			                                        os.getenv('RZP_DOMESTIC_WEBHOOK_SECRET'))
+		except SignatureVerificationError:
+			return Response({'status': 'false', 'detail': 'Webhook signature verification error.'}, status=424)
+
 		print(request.data)
-		payment = request.data['payload']['payment']['entity']
-		donor = payment.get('notes')
-		Donation.objects.create(rzp_payment_id=payment.get('id'), amount=int(payment.get('amount')),
-		                        currency=payment.get('currency'), donor_name=donor.get('name'),
-		                        donor_email=donor.get('email'), donor_pan=donor.get('pan'),
-		                        donor_address=donor.get('address'), donor_country=donor.get('donor_country'),
-		                        donor_zipcode=donor.get('zipcode'),
-		                        payment_time=datetime.fromtimestamp(int(payment['payload']['payment']['entity']['created_at']), timezone.utc),
-		                        rzp_response=request.data, domestic=True, international=False, success=payment.get('captured'))
-
-
-		# client = razorpay.Client(auth=(os.getenv('RZP_KEY'), os.getenv("RZP_SECRET_KEY")))
-		# try:
-		# 	client.utility.verify_webhook_signature(request.data, webhook_signature, os.getenv('RZP_DOMESTIC_WEBHOOK_SECRET'))
-		# except SignatureVerificationError:
-		# 	return Response({'status': 'false', 'detail': 'Webhook signature verification error.'})
+		if request.data['event'] == 'payment.captured' or request.data['event'] == 'payment.failed':
+			payment = request.data['payload']['payment']['entity']
+			donor = payment.get('notes')
+			Donation.objects.create(rzp_payment_id=payment.get('id'), amount=int(payment.get('amount')),
+			                        currency=payment.get('currency'), donor_name=donor.get('name'),
+			                        donor_email=donor.get('email_address'), donor_pan=donor.get('pan_number'),
+			                        donor_address=donor.get('address'), donor_country=donor.get('donor_country'),
+			                        donor_zipcode=donor.get('zipcode'), donor_phone=donor.get('phone'),
+			                        payment_time=datetime.fromtimestamp(int(payment['payload']['payment']['entity']['created_at']), timezone.utc),
+			                        rzp_response=request.data, domestic=True, international=False, success=payment.get('captured'))
+			print("success")
 		return Response(200)
